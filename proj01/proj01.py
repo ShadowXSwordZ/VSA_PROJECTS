13d980482c8c7f70bbe9214687a2bf0935562484
Peabody <peabody@pbdy-m-wc132-ap.local>
2017-06-12 11:40:57 -0500

    proj1

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -5,6 +5,16 @@
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
-# If you complete extensions, describe your extensions here!
+# If you complete extensions, describe your extensions here
 
+
+
+user_input = raw_input("Enter your name: ")
+
+user_input2 = raw_input("Enter your age: ")
+
+part_1 = "will be 100 years old in"
+part_2 = "years, which will be the year"
+part_3 = 100 - int(user_input2)
+print user_input, part_1, 1
 
